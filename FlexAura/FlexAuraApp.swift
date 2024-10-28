694cd36d5199ec06f0c50268a34988aec6da0bc0
Krish Kharbanda <kharbandakrish23@gmail.com>
2024-10-27 22:39:57 -0400

    Initial Commit

---
+++ FlexAura/FlexAuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexAuraApp.swift
+//  FlexAura
+//
+//  Created by Krish on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FlexAuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlexAuraTests/FlexAuraTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlexAuraTests.swift
+//  FlexAuraTests
+//
+//  Created by Krish on 10/27/24.
+//
+
+import Testing
+@testable import FlexAura
+
+struct FlexAuraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
